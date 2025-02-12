97848c18b8082d33747f7729aaa907c3c2974f07
Piyush Kumar <piyush.acet@gmail.com>
2025-02-12 14:36:00 +0530

    update readme

---
+++ code-practice/remove-duplicate.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(arr []int) []int {
+	var newArr []int
+	arrMap := make(map[int]bool)
+
+	for _, v := range arr {
+		if _, ok := arrMap[v]; !ok {
+			arrMap[v] = true
+			newArr = append(newArr, v)
+		}
+	}
+	return newArr
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 10, 9, 6, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(removeDuplicates(arr))
+}
